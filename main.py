1402cfe44689ce852eec1f2a3ea3f9f251851950
Hood12345 <hcosubmissions@gmail.com>
2025-05-25 15:18:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -61,8 +61,8 @@ def edit_video():
 
         # Step 4: Place caption just above the video (on top white pad)
         drawtext = (
-            f"drawtext=fontfile='{FONT_PATH}':text='{caption}':"
-            f"fontcolor=black:fontsize=48:x=(w-text_w)/2:y=40"
+drawtext=fontfile='{FONT_PATH}':text='{caption}':fontcolor=black:fontsize=48:x=(w-text_w)/2:y=((oh-ih)/2 - text_h - 20)
+
         )
 
         vf_filters = f"{scale_and_pad},{drawtext}"
